f981f37baf7e761604c2c80ad81be7195289283a
Benjamin Drejer <benjadrejer@gmail.com>
2017-12-17 19:08:11 +0100

    Add project files.

---
+++ InheritanceExercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            stack.Clear();
+            stack.Push(4);
+            Console.WriteLine(stack.Pop());
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ InheritanceExercise/Stack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceExercise
+{
+    public class Stack
+    {
+        private ArrayList _stack = new ArrayList();
+        
+        public void Push(object item)
+        {
+            if(item == null)
+            {
+                throw new NullReferenceException("You cannot add a null reference to the stack");
+            }
+            else
+            {
+                _stack.Add(item);
+            }
+        }
+
+        public object Pop()
+        {
+            if(_stack.Count > 0)
+            {
+                Object item = _stack[_stack.Count - 1];
+                _stack.RemoveAt(_stack.Count - 1);
+                return item;
+            }
+            throw new InvalidOperationException("You cannot pop an empty stack");
+
+        }
+
+        public void Clear()
+        {
+            _stack = new ArrayList();
+        }
+    }
+}
